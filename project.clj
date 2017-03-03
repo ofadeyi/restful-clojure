63412b904f9b41f9c25082f66f2dfb41b05de09a
ofadeyi <ofadeyi@gmail.com>
2017-03-03 01:43:44 +0000

    LEARN-001 | Ola | Updated project.clj with ring/compojure required infrastructure

--- project.clj
+++ project.clj
@@ -4,8 +4,15 @@
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  
+
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.5.0"]
                  [ring/ring-jetty-adapter "1.5.0"]
-                 [compojure "1.5.1"]])
+                 [compojure "1.5.1"]]
+
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler restful-clojure.core/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}}
+  )
