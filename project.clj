74672650e1cc4ff71e4aa6b51a56a95dc2a1179d
ofadeyi <ofadeyi@gmail.com>
2017-03-03 01:22:13 +0000

    LEARN-001 | Ola | Added dependencies (ring, compojure)

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject restful-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Following the 'Writing a RESTful web service in Clojure'"
+  :url "https://kendru.github.io/restful-clojure/2014/02/16/writing-a-restful-web-service-in-clojure-part-1-setup/"
+  :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-core "1.5.0"]
+                 [ring/ring-jetty-adapter "1.5.0"]
+                 [compojure "1.5.1"]])
