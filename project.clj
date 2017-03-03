cf3e51d9d2d16a167d56cbcb0f6954054ea417cc
ofadeyi <ofadeyi@gmail.com>
2017-03-03 23:52:04 +0000

    LEARN-002 | Ola | Using Ring-Json middleware to transform json to and from clojure data structures

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.5.0"]
                  [ring/ring-jetty-adapter "1.5.0"]
-                 [compojure "1.5.1"]]
+                 [compojure "1.5.1"]
+                 [ring/ring-json "0.4.0"]]
 
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler restful-clojure.handler/app}
--- src/restful_clojure/handler.clj
+++ src/restful_clojure/handler.clj
@@ -1,10 +1,13 @@
 (ns restful-clojure.handler
   (:require [compojure.core :refer :all]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [ring.util.response :refer [response]]
+            [ring.middleware.json :refer :all]))
 
 ;; Routes
 (defroutes app-routes
-           (route/not-found "{\"message\":\"Page not found\"}"))
+           (route/not-found
+             (response {:message "Page not found"}) ))
 
 ;; Middleware
 (defn wrap-log-info [handler]
@@ -15,4 +18,6 @@
 ;; Main Handler
 (def app
     (-> app-routes
-        wrap-log-info))
+        wrap-log-info
+        wrap-json-response
+        wrap-json-body))
