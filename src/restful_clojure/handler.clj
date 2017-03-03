4ddf749893da5a6fa0d86bc957bac58cb010c6eb
ofadeyi <ofadeyi@gmail.com>
2017-03-03 23:22:47 +0000

    LEARN-002 | Ola | Added ‘not-found’ route handler

--- src/restful_clojure/handler.clj
+++ src/restful_clojure/handler.clj
@@ -1,4 +1,8 @@
 (ns restful-clojure.handler
-  (:require [compojure.core :refer :all]))
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]))
 
-(defroutes app )
+(defroutes app-routes
+           (route/not-found "{\"message\":\"Page not found\"}"))
+
+(def app app-routes)
